906bd0802645b98bea6b0c27c6692cbcd38f4b88
Lauberr <130068637+Lauberr@users.noreply.github.com>
2024-08-24 23:06:12 -0300

    criando divisoes do site
    continuando projeto React, aprendendo a como criar snippets e usá-los no projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { Main } from "./components/Main";
-
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
 
 export function App(){
-  return(
+  return (
     <>
       <Header/>
-      <Main/>
-      <Footer/>
+      <Main>
+            <h3>Conteúdo</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam perspiciatis, ullam reprehenderit voluptates voluptatem, assumenda quibusdam, earum soluta nesciunt iusto autem odit libero perferendis doloribus enim beatae iure vel quasi.</p> 
+      </Main>
+      <Footer autor="Lauber" ano="2024"></Footer>
     </>
   )
 }
+
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import styles from './Main.module.css'
+
+export function Main(props) {
+    const {children} = props
+
+    return (
+        <div className={styles.Main}>
+            {children}
+        </div>
+    )
+}
